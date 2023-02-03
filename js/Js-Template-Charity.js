df41970bdcd4253cd8da29ad8aa6093906c8898c
SonSalem <salemmamdouh77777@gmail.com>
2023-02-03 16:08:29 +0200

    Uploding Main File

---
+++ js/Js-Template-Charity.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".owl-banner").owlCarousel({
+      loop:true,
+      dots:true,
+      autoplayTimeout:4000,
+      autoplay : true ,
+      items:1,
+      }
+    );
+  });
+$(document).ready(function(){
+    $(".owl-donate").owlCarousel({
+      loop:true,
+      nav:true,
+      autoplayTimeout:2000,
+      autoplay : true ,
+      items:1,
+      }
+    );
+  });
